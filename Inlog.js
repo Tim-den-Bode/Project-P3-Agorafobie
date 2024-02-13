4798d0ac1856558f2aabf2c68109e5f3fae9bed6
Tokiodan <danibouzidi7@gmail.com>
2024-02-13 09:58:27 +0100

    DanisFixedCommit

---
+++ Inlog.js
@@ -0,0 +1,12 @@
+document.querySelector('input[type="submit"]').addEventListener('click', function(event) {
+    event.preventDefault();
+  
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+  
+    if (username === 'testuser' && password === 'testpassword') {
+      window.location.href = "index.html";
+    } else {
+      alert('Verkeerde gebruikersnaam of wachtwoord');
+    }
+}  
