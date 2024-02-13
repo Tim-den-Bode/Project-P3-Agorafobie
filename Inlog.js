b9dc3f8142b47823acca871b264378a8c961b621
Tokiodan <danibouzidi7@gmail.com>
2024-02-13 12:21:46 +0100

    DanisDerdeCommit

--- Inlog.js
+++ Inlog.js
@@ -10,3 +10,22 @@ document.querySelector('input[type="submit"]').addEventListener('click', functio
     alert('Verkeerde gebruikersnaam of wachtwoord');
   }
 });
+
+const body = document.body;
+const backgroundImages = [
+  'https://source.unsplash.com/random/1600x900/?mountains,landscape',
+  'https://source.unsplash.com/random/1600x900/?nature,forest',
+  'https://source.unsplash.com/random/1600x900/?ocean,beach',
+  'https://source.unsplash.com/random/1600x900/?city,urban',
+  'https://source.unsplash.com/random/1600x900/?space,stars',
+  'https://source.unsplash.com/random/1600x900/?desert,sand'
+];
+
+let currentImageIndex = 0;
+
+function changeBackground() {
+  body.style.backgroundImage = `url('${backgroundImages[currentImageIndex]}')`;
+  currentImageIndex = (currentImageIndex + 1) % backgroundImages.length;
+}
+
+setInterval(changeBackground, 5000);
