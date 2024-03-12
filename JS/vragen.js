4e6fd297aae6f15304dbe36962ec7e8d86047762
Tokiodan <danibouzidi7@gmail.com>
2024-03-12 09:53:20 +0100

    DanisCommitVragenLijst

--- JS/vragen.js
+++ JS/vragen.js
@@ -3,3 +3,11 @@ var currentPage = window.location.pathname;
 function nextPage() {
     window.location.href = 'vragen2.html';
 }
+
+function previousPage() {
+    window.location.href = 'vragen.html';
+}
+
+/* function donePage() {
+    window.location.href = "Toekomstige uitslag site";
+} */
--- db/db.sql
+++ db/db.sql
@@ -1,11 +1,6 @@
 create database AgaroFobie;
 USE AgaroFobie;
 
-CREATE USER 'Dani'@'localhost' IDENTIFIED BY '1234';
-
-GRANT ALL PRIVILEGES ON AgaroFobie.* TO 'Dani'@'localhost';
-
-FLUSH PRIVILEGES;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
