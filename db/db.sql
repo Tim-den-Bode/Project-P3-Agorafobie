1dad980734546cf9019197776d41693864b81b35
Tim-den-Bode <330583@student.mboutrecht.nl>
2024-03-26 09:54:39 +0100

    Update db.sql
    email is allowed to be empty

--- db/db.sql
+++ db/db.sql
@@ -5,7 +5,7 @@ USE AgaroFobie;
 CREATE TABLE IF NOT EXISTS `users` (
     `id` int NOT NULL AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
-    `email` varchar(255) NOT NULL,
+    `email` varchar(255),
     `answers` text,
     `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
     `password` varchar(255) NOT NULL,
