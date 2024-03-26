ba17c77e3c1ef47223bbf4a7db826f7355b2fb98
Tim-den-Bode <330583@student.mboutrecht.nl>
2024-03-26 09:08:28 +0100

    Update db.sql
    updated the database

--- db/db.sql
+++ db/db.sql
@@ -1,9 +1,13 @@
 create database AgaroFobie;
+
 USE AgaroFobie;
 
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `answers` text,
+    `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    `password` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
 );
