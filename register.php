51bda4d7d111359a147c04935e3415051b687831
TimLigteringen <141249874+GalaxyGhostBat@users.noreply.github.com>
2024-03-29 16:34:51 +0100

    Update register.php
    code clean up

--- register.php
+++ register.php
@@ -1,28 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agoraphobia User Registration</title>
-    <link rel="stylesheet" type="text/css" href="css/Register.css" />
+    <link rel="stylesheet" type="text/css" href="css/Register.css"/>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="JS/Register.js"></script>
 </head>
-
 <body>
     <div id="bg-container">
-        <img src="img/ditism.jpg" alt="" />
-        <img src="img/schoolp123.jpg" alt="" />
-        <img src="img/martjiachte1.jpg" alt="" />
+        <img src="img/ditism.jpg" alt=""/>
+        <img src="img/schoolp123.jpg" alt=""/>
+        <img src="img/martjiachte1.jpg" alt=""/>
     </div>
     <div class="register-container">
         <h1>Welcome to PhobiaHelp</h1>
         <p>Please sign up to create your account and access your personalized results.</p>
         <h2>Register</h2>
         <form action="PHP/Lib/register_handler.php" method="post">
-            <label for="username">username:</label><br>
-            <input type="text" id="username" name="username"><br>
+            <label for="name">Name:</label><br>
+            <input type="text" id="name" name="name"><br>
             <label for="email">Email:</label><br>
             <input type="email" id="email" name="email"><br>
             <label for="password">Password:</label><br>
@@ -36,5 +34,4 @@
         </footer>
     </div>
 </body>
-
 </html>
