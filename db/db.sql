4ab1496f9a29d92d23c9399f65a515ff3e406627
TimLigteringen <141249874+GalaxyGhostBat@users.noreply.github.com>
2024-03-29 15:33:09 +0100

    Update db.sql
    updated what is saved in the database

--- db/db.sql
+++ db/db.sql
@@ -2,12 +2,14 @@ create database agorafobie;
 
 USE agorafobie;
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) NOT NULL,
-    `email` varchar(255),
-    `answers` text,
-    `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    `password` varchar(255) NOT NULL,
-    PRIMARY KEY (`id`)
-);
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `answers` text,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+)
+
+
